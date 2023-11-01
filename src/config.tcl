2d35b69288f78741157a80c7eecbbe7f4f87bb4e
Daniel Barrios <68023784+DanielBarrios2190@users.noreply.github.com>
2023-11-01 00:42:32 -0500

    New Branch

---
+++ src/config.tcl
@@ -0,0 +1,17 @@
+set ::env(DESIGN_NAME) src
+
+set ::env(VERILOG_FILES) "\
+	$::env(DESIGN_DIR)/src.v"
+
+set ::env(DESIGN_IS_CORE) 0
+
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_NET) "clk"
+set ::env(CLOCK_PERIOD) "15.0"
+
+set ::env(FP_SIZING) "absolute"
+set ::env(DIE_AREA) "0 0 220 220" 
+
+set ::env(PL_TARGET_DENSITY) 0.8
+set ::env(FP_CORE_UTIL) 100
+ 
