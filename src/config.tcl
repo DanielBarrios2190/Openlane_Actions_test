4f55286c6e6fba5244cba8158c6f01f57754754e
Daniel Barrios <68023784+DanielBarrios2190@users.noreply.github.com>
2023-11-01 01:04:42 -0500

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -10,7 +10,7 @@ set ::env(CLOCK_NET) "clk"
 set ::env(CLOCK_PERIOD) "15.0"
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) "0 0 220 220" 
+set ::env(DIE_AREA) "0 0 300 300" 
 
 set ::env(PL_TARGET_DENSITY) 0.6
 set ::env(FP_CORE_UTIL) 100
