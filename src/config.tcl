e97fe9f3686f2b712742ba6bbfa1125ba178b393
Jorge Angarita Pérez <68038923+Gior-gio@users.noreply.github.com>
2023-11-03 17:29:06 -0500

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -19,7 +19,7 @@ set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) "clk"
 set ::env(CLOCK_PERIOD) "15.0"
 
-#set ::env(FP_SIZING) "absolute"
+set ::env(FP_SIZING) "relative"
 #set ::env(DIE_AREA) "0 0 200 200" 
 
 set ::env(PL_TARGET_DENSITY) 0.9
