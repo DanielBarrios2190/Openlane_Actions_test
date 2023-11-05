83f8cb258dba8a305b3318af72378109951c0123
Daniel Barrios <68023784+DanielBarrios2190@users.noreply.github.com>
2023-11-05 15:51:11 -0500

    Probar Nuevos nombres

--- src/config.tcl
+++ src/config.tcl
@@ -1,16 +1,16 @@
 set ::env(DESIGN_NAME) tt_um_src
 
 set ::env(VERILOG_FILES) "\
-	$::env(DESIGN_DIR)/tt_um_src.v"
+	$::env(DESIGN_DIR)/src.v"
  
 set ::env(DESIGN_IS_CORE) 0
 
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) "clk"
-set ::env(CLOCK_PERIOD) "15.0"
+set ::env(CLOCK_PERIOD) "20.0"
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) "0 0 650 650" 
+set ::env(DIE_AREA) "0 0 200 200" 
 
 set ::env(PL_TARGET_DENSITY) 0.75
 set ::env(FP_CORE_UTIL) 85
